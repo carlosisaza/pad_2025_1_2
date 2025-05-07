2ae3252a6d003b5b9001e571eb78e2d25617bf7f
Carlos Isaza <carlosisaza21@hotmail.com>
2025-05-06 23:09:22 -0500

    empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="edu_pad",
+    version="0.0.1",
+    author="Carlos Isaza",
+    author_email="carlosisaza21@hotmail.com",
+    description="Proyecto realizado en clase Mayo 2025",
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4",
+    ]
+)
